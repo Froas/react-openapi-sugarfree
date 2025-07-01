b0deb0051a8b1c38e84e5ed207dc477d3c17816e
froas <froas.crp@gmail.com>
2025-07-01 23:40:54 +0900

    Create a project

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="flex items-center justify-center min-h-screen bg-gradient-to-br from-blue-600 via-purple-500 to-pink-400">
+      <h1 className="text-5xl font-bold text-white shadow-lg p-8 rounded-3xl bg-black/40">
+        Hello World
+      </h1>
+    </div>
+    </>
+  )
+}
+
+export default App
